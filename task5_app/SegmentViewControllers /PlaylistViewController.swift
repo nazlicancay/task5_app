7846b562c4bad4f7f5dcf386c5367f26b2c3cdb1
Nazlıcan Çay <nazlican1306@gmail.com>
2023-09-04 14:14:22 +0300

    segment control and general uı

---
+++ task5_app/SegmentViewControllers /PlaylistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  playlistViewController.swift
+//  task5_app
+//
+//  Created by Nazlıcan Çay on 4.09.2023.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ task5_app/SegmentViewControllers /SoundViewController.swift
@@ -0,0 +1,30 @@
+//
+//  soundViewController.swift
+//  task5_app
+//
+//  Created by Nazlıcan Çay on 4.09.2023.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
